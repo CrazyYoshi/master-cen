27349ab4cd812cf2ce5fe481b5687550a360b74b
Miloud Maamar <development@maamar.fr>
2020-11-19 09:43:12 +0100

    tag images

--- run-master-cen.ps1
+++ run-master-cen.ps1
@@ -1,4 +1,4 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker build $PSScriptRoot  -t master-cen:latest
-docker run -d -p 80:80 -p 81:81 -p 82:82 -p 83:83 -p 84:84 -p 85:85 master-cen:latest
+docker build $PSScriptRoot  -t crazyyoshi/master-cen:latest
+docker run -d -p 80:80 -p 81:81 -p 82:82 -p 83:83 -p 84:84 -p 85:85 crazyyoshi/master-cen:latest
